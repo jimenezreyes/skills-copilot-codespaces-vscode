e4455cdb6e851f732796ac3283e75f1cc4067a9c
abraham jimenez reyes <74029995+jimenezreyes@users.noreply.github.com>
2023-11-01 21:24:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,70 @@
+// Path: member.js
+function skillsMember2()
+{
+    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
+    document.getElementById("skills").innerHTML = skills[4];
+}
+
+// Path: member.js
+function skillsMember3()
+{
+    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
+    document.getElementById("skills").innerHTML = skills[0];
+}
+
+// Path: member.js
+function skillsMember4()
+{
+    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
+    document.getElementById("skills").innerHTML = skills[1];
+}
+
+// Path: member.js
+function skillsMember5()
+{
+    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
+    document.getElementById("skills").innerHTML = skills[3];
+}
+
+// Path: member.js
+function skillsMember6()
+{
+    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
+    document.getElementById("skills").innerHTML = skills[2];
+}
+
+// Path: member.js
+function skillsMember7()
+{
+    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
+    document.getElementById("skills").innerHTML = skills[4];
+}
+
+// Path: member.js
+function skillsMember8()
+{
+    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
+    document.getElementById("skills").innerHTML = skills[0];
+}
+
+// Path: member.js
+function skillsMember9()
+{
+    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
+    document.getElementById("skills").innerHTML = skills[1];
+}
+
+// Path: member.js
+function skillsMember10()
+{
+    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
+    document.getElementById("skills").innerHTML = skills[3];
+}
+
+// Path: member.js
+function skillsMember11()
+{
+    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
+    document.getElementById("skills").innerHTML = skills[2];
+}
+
